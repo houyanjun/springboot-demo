c6b4d4bc58d7177d513724be7210f6a8441ff921
houyanjun <18520164621@163.com>
2022-05-29 02:04:39 +0800

    整合了nacos方案

--- src/main/java/com/example/demo/api/HelloApiService.java
+++ src/main/java/com/example/demo/api/HelloApiService.java
@@ -1,18 +1,25 @@
 package com.example.demo.api;
 
 import com.example.demo.api.dto.DemoDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@RefreshScope
 public class HelloApiService {
+
+    @Value("${name}")
+    private String name;
 
     @PostMapping(value="/test")
     public DemoDTO hello() {
         DemoDTO demoDTO = new DemoDTO();
         demoDTO.setName("hou");
+        System.out.println(name);
         return demoDTO;
     }
 }
